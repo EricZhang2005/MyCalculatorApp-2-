4e33f11c1d1b7eb2346f345fc1e031cbebc8758d
YT65a <65a.business@gmail.com>
2021-08-25 20:45:54 -0500

    8-25-21 before attempting dark mode

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -17,18 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // code provided by "https://www.javatpoint.com/android-custom-toast-example"
-        /*LayoutInflater li = getLayoutInflater();
-        //Getting the View object as defined in the customtoast.xml file
-        View layout = li.inflate(R.layout.activity_main,(ViewGroup) findViewById(R.id.custom_toast_layout));
-
-        //Creating the Toast object
-        Toast toast = new Toast(getApplicationContext());
-        toast.setDuration(Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-        toast.setView(layout);//setting the view of custom toast layout
-        toast.show(); */
     }
 
     public void add(View v) {
